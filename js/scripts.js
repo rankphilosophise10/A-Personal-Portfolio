a1e5db5dca3331c94f304c148aeeef8bd528562c
rankphilosophise10 <nathanieljclendenin@gmail.com>
2025-09-05 17:04:01 -0400

    feat
    Additional changes to scripts.js for headshot image

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,14 @@
-const headshotWrap = document.createElement("div");
-  headshotWrap.className = "headshotContainer";
+const headshot = createHeadshotContainer("../images/headshot.jpg", "User headshot");
+document.body.appendChild(headshot);
+
+function createHeadshotContainer(src, altText) {
+  const wrap = document.createElement("div");
+  wrap.className = "headshotContainer";
+
+  const img = document.createElement("img");
+  img.src = src;
+  img.alt = altText;
+
+  wrap.appendChild(img);
+  return wrap;
+}
